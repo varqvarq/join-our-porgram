76a8fd0b19e7f04e8cd9b7112f8dd3f1ceec7659
Ramil Askarov <crouzch1@gmail.com>
2024-02-14 13:01:27 +0600

    Fix

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-import { JoinUsSection } from './join-us-section.js';
+  import { JoinUsSection } from './join-us-section.js';
 
-class SectionCreator {
-  create(type) {
-    switch (type){
-      case 'standard':
-        return new JoinUsSection().createSection();
-      case 'advanced':
-        return new JoinUsSection('Join Our Advanced Program', 'Subscribe to Advanced Program').createSection();
-      default: 
-        throw new Error('Invalid section type')
+  class SectionCreator {
+    create(type) {
+      switch (type){
+        case 'standard':
+          return new JoinUsSection().createSection();
+        case 'advanced':
+          return new JoinUsSection('Join Our Advanced Program', 'Subscribe to Advanced Program').createSection();
+        default: 
+          throw new Error('Invalid section type')
+      }
     }
+
   }
 
-}
+  const sectionCreator = new SectionCreator();
+  const standardSection = sectionCreator.create('standard')
+  const advancedSection = sectionCreator.create('advanced');
 
-const sectionCreator = new SectionCreator();
-const standardSection = sectionCreator.create('standard')
-const advancedSection = sectionCreator.create('advanced');
+  const footer = document.querySelector('.app-footer');
+  footer.before(advancedSection);
 
-const footer = document.querySelector('.app-footer');
-footer.before(advancedSection);
 
